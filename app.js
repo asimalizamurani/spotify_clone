3ed8189fcd31cd733d357aedbf5bf5af14dada63
sobanyousuf4 <sobanyousuf4@gmail.com>
2024-02-02 12:42:44 +0500

    Today i added the hover functionality on the cards

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ let cards = document.querySelectorAll(".card");
 let playIcon = document.querySelectorAll(".play-icon");
 
 cards.forEach((card) => {
-    card.addEventListener('click', () => {
-        console.log("clicked bro");
-        card.classList.remove('play-icon');
+    card.addEventListener("mouseenter", function(md) {
+       playIcon.forEach((icon) => {
+        // icon.style.opacity = 1;
+        icon.addEventListener("mouseenter", () => {
+            icon.style.opacity = 1;
+        //     op.style.opacity = 1;
+        })
+        
+       })  
     })
 })
+
+cards.forEach((card) => {
+    card.addEventListener("mouseleave", function(md) {
+       playIcon.forEach((icon) => {
+        icon.style.opacity = 0;
+       })  
+    })
+})
+
+
